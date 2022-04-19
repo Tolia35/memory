8ca88dfb7989e1f37cff32f759a7f4854e50d4eb
tolia <anatolia.maupile@insaniam.fr>
2022-04-19 12:02:32 +0200

    first commit

---
+++ src/components/SingleCards.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './SingleCards.css';
+
+const SingleCards = ({card, handleChoice, flipped, disabled }) => {
+
+    const handleClick = () => {
+        if (!disabled) {
+            handleChoice(card)
+        }
+    }
+
+    return (
+        <div className="card">
+            <div className={flipped ? "flipped" : ""}>
+                <img
+                    className="front"
+                    src={card.src}
+                    alt="card front"
+                />
+                <img
+                    className="back"
+                    onClick={handleClick}
+                    src="/img/cover.png"
+                    alt="card back"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default SingleCards;
